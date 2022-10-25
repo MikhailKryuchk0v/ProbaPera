734707f8a978b02d79c615bcfcd9da8dfb184b02
MikhailKryuchk0v <114687275+MikhailKryuchk0v@users.noreply.github.com>
2022-10-25 15:51:08 +0300

    Add files via upload

---
+++ PR6_1 var1.py
@@ -0,0 +1,7 @@
+arr=[]
+n=5
+for i in range (n):
+    arr.append(input('Введие число:'))
+print('Max element: '+ str(max(arr)))
+print('Obratnii poriadok: '+ str(list(reversed(arr)))) 
+   
---
+++ PR6_2 var 1.py
@@ -0,0 +1,11 @@
+a = list(map(float,input().split()))
+s=00
+for i in range (len(a)):
+    s+=a[i]
+    av=s/len(a)
+for i in  range(len(a)):
+    if a[i]==0:
+        a[i] = av
+print(a[i], end="")
+    
+

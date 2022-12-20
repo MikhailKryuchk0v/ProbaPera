9497e74c927385a5815787809b6457250fb61ed5
MikhailKryuchk0v <114687275+MikhailKryuchk0v@users.noreply.github.com>
2022-12-20 13:59:57 +0300

    Add files via upload

---
+++ main-14.py
@@ -0,0 +1,26 @@
+import csv
+from typing import Iterable, Callable
+
+
+# Реализация собственной функции фильтрации последовательности элементов по заданному предикату(условию)
+def my_filter(it: Iterable, predicate: Callable[[any], bool]) -> Iterable:
+    return [i for i in it if predicate(i)]
+
+
+# Реализация собственной функции подсчета кол-ва элеметов в последовательности
+def my_count(it: Iterable) -> int:
+    count = 0
+    return [count := count + 1 for _ in it][-1]
+
+
+# Открываем файл с данными в формате csv
+with open('StudentsPerformance 14.csv', 'r') as f:
+    # Инициализируем csv reader
+    reader = csv.DictReader(f)
+    # Считываем всех студентов из csv
+    students = [s for s in reader]
+    # Фильтруем студентов согласно условию задачи
+    coursed_males = my_filter(students, lambda s: s['gender'] == 'male' and s['test preparation course'] == 'completed')
+    # Выводим ответ - кол-во отфильтрованных элеметов (соответствующих заданному условию)
+    print(my_count(coursed_males))
+    pass
